e3b673d6664457a5513abf93181279137cc87ce5
Donald Newholm <newholm1@hotmail.com>
2019-11-27 17:01:03 +0000

    Midway through Step 3, 6 passing tests

---
+++ spec/note-list-view-spec.js
@@ -0,0 +1,18 @@
+function doesTheNoteListViewStoreTheNoteListModel() {
+  var notelist = new NoteList();
+  var notelistview = new NoteListView(notelist);
+  assert.isTrue((notelistview.notelist) === (notelist));
+};
+
+doesTheNoteListViewStoreTheNoteListModel();
+
+function doesItReturnAStringOfHTMLThatRepresentsTheNoteListModel() {
+  var notelist = new NoteList();
+  notelist.createNote("Oh God");
+  var notelistview = new NoteListView(notelist);
+  console.log(notelist);
+  assert.isTrue(notelistview.htmlDisplay() === "<ul><li><div>Oh God</div></li></ul>");
+
+};
+
+doesItReturnAStringOfHTMLThatRepresentsTheNoteListModel();
