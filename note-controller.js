a4e88bf632c35878368cf70c601979d677a722b6
Donald Newholm <newholm1@hotmail.com>
2019-11-29 16:09:32 +0000

    Step 5 spiked with GREAT SUCCESS

--- note-controller.js
+++ note-controller.js
@@ -1,3 +1,23 @@
-window.onload = function() {
-document.getElementById("app").innerHTML = "howdy";
-}
+function NoteController() {
+  this.noteList = new NoteList;
+  this.noteListView = new NoteListView(this.noteList);
+};
+
+NoteController.prototype = (function() {
+
+  function addHTML(text) {
+    this.noteList.createNote(text);
+    content = this.noteListView.htmlDisplay();
+    window.onload = function() {
+      document.getElementById("app").innerHTML = content;
+    }
+  };
+
+  return {
+    addHTML: addHTML
+  };
+
+})();
+
+noteController = new NoteController();
+noteController.addHTML("Favourite drink: seltzer, after the salsa");
